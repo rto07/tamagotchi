18732354f9f11f4a5bf57d924dc573f69e69f4eb
rto07 <tar845@Rauls-MacBook-Pro.local>
2017-09-25 15:40:07 -0500

    starter files

---
+++ script/apps.js
@@ -0,0 +1,46 @@
+// Tomagotchi
+
+// Description
+
+// Let's use our front-end web development skills to create a "living" pet! We'll use HTML, CSS, and Javascript to interact with our pet.
+
+
+// Requirements
+
+// Create a repo for your tomagotchi pet
+
+// make a commit after you finish each one of the following
+
+// Create a class for you tomagotchi
+
+// Display a character of your choice on the screen to represent your pet
+
+// Display the following metrics for your pet. Hunger (1-10 scale), Sleepiness (1-10 scale), Boredom (1-10 scale), Age
+
+// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet
+
+// Add the ability to name your pet
+
+// Style the page
+
+// Increase your pet's age every x minutes
+
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+
+// Morph your pet at certain ages
+
+// Animate your pet across the screen
+
+
+class tamagotchi{
+	constructor(name, clothingColor, closet, lives, attacks){
+		this.name = name;
+		this.closet = closet;
+		this.clothingColor = clothingColor;
+		this.lives = lives;
+		this.attacks = attacks;
+		this.attack = {};
+		this.damageMultiplier = 0;
+	}
